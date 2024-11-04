316f1c0ef67c7a34503bc6cb90ddb66f8f6f81a9
pyeonjoy <ppee1220@naver.com>
2024-11-04 11:25:15 +0900

    모바일

--- src/app/components/body.js
+++ src/app/components/body.js
@@ -25,7 +25,6 @@ export default function Body() {
         try {
             const response = await axios.get(todoUrl);
             setTodoList(response.data);
-            // 데이터가 성공적으로 로드되면 로컬 스토리지에 저장
             localStorage.setItem("todoList", JSON.stringify(response.data));
         } catch (error) {
             console.error("Error fetching todo list:", error);
@@ -33,12 +32,11 @@ export default function Body() {
     };
 
     useEffect(() => {
-        // 로컬 스토리지에 저장된 데이터가 있다면 먼저 로드
         const storedTodos = localStorage.getItem("todoList");
         if (storedTodos) {
             setTodoList(JSON.parse(storedTodos));
         } else {
-            fetchData(); // 데이터가 없다면 서버에서 가져오기
+            fetchData();
         }
     }, []);
 
@@ -51,7 +49,7 @@ export default function Body() {
         try {
             await axios.post(todoUrl, { text });
             setText("");
-            fetchData(); // 데이터베이스에서 최신 todo 리스트를 가져옵니다.
+            fetchData();
             setIsModalOpen(false);
 
             const randomImageIndex = Math.floor(Math.random() * images.length);
@@ -62,6 +60,7 @@ export default function Body() {
             console.error("Error adding todo:", error);
         }
     };
+
     const handleCircleClick = (index) => {
         setActiveTextIndex(index);
     };
@@ -75,7 +74,7 @@ export default function Body() {
 
     const handleModalClose = () => {
         setIsModalOpen(false);
-        setText(""); // 모달을 닫을 때 입력 필드 초기화
+        setText("");
     };
 
     const handleTextChange = (e) => {
@@ -83,119 +82,97 @@ export default function Body() {
     };
 
     return (
-        <>
-            <div className="flex-grow flex items-center justify-center p-4" onClick={handleModalOpen} style={{ backgroundImage: 'url("/back.png")', backgroundSize: 'cover' }}>
-                <div className="w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl h-[600px] md:h-[700px] flex flex-col">
-                    <div className="flex-1 overflow-y-auto p-4">
-                        {todoList.map((todo) => (
-                            <div key={todo.id} className="flex flex-col">
-                                <div className="chat chat-end">
-                                </div> 
-                            </div>
-                        ))}
-                        <div ref={endOfListRef} />
-                    </div>
+        <div
+            className="flex-grow flex items-center justify-center p-4 relative"
+            onClick={handleModalOpen}
+            style={{ backgroundImage: 'url("/back.png")', backgroundSize: 'cover' }}
+        >
+            <div className="w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl h-[500px] sm:h-[600px] md:h-[700px] flex flex-col items-center justify-center">
+                <div className="flex-1 overflow-y-auto p-4">
+                    {todoList.map((todo) => (
+                        <div key={todo.id} className="flex flex-col">
+                            <div className="chat chat-end">
+                            </div> 
+                        </div>
+                    ))}
+                    <div ref={endOfListRef} />
                 </div>
-
-                {circles.map((circle, index) => (
-                    <img
-                        key={index}
-                        src={circle.image}
-                        alt="Circle Image"
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            handleCircleClick(index);
-                        }}
-                        style={{
-                            position: "absolute",
-                            left: circle.position.x - 25,
-                            top: circle.position.y - 25,
-                            width: 50,
-                            height: 50,
-                            cursor: "pointer",
-                        }}
-                    />
-                ))}
-
-                {activeTextIndex !== null && (
-                    <div
-                        style={{
-                            position: "absolute",
-                            left: circles[activeTextIndex].position.x + 60,
-                            top: circles[activeTextIndex].position.y - 25,
-                            backgroundColor: "white",
-                            padding: "8px",
-                            border: "1px solid #e5e7eb",
-                            borderRadius: "4px",
-                        }}
-                    >
-                        {circles[activeTextIndex].text}
-                        <button
-                            onClick={() => setActiveTextIndex(null)}
-                            className="text-red-500 hover:text-red-700 ml-2"
-                        >
-                            X
-                        </button>
-                    </div>
-                )}
-
-                {isModalOpen && (
-                    <div
-                        style={{
-                            position: "absolute",
-                            left: modalPosition.x,
-                            top: modalPosition.y,
-                            backgroundColor: "white",
-                            border: "1px solid #e5e7eb",
-                            padding: "15px",
-                            borderRadius: "8px",
-                            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-                        }}
-                    >
-                        <button
-                            onClick={handleModalClose}
-                            className="text-gray-500 hover:text-gray-700 absolute top-2 right-2"
-                            style={{
-                                marginRight: "7px",
-                                borderRadius: "50%",
-                            }}
-                        >
-                            X
-                        </button>
-                        <form onSubmit={onSubmitHandler}>
-                            <div>
-                                <input
-                                    name="text"
-                                    type="text"
-                                    placeholder={t("message")}
-                                    className="input w-full"
-                                    value={text}
-                                    style={{
-                                        marginTop: "30px",
-                                    }}
-                                    onChange={handleTextChange}
-                                />
-                            </div>
-                            <input
-                                type="submit"
-                                value={t("submit")}
-                                className="btn"
-                                style={{
-                                    backgroundColor: "yellow",
-                                    color: "black",
-                                    width: "100%",
-                                    border: "none",
-                                    borderRadius: "14px",
-                                    padding: "2px",
-                                    cursor: "pointer",
-                                    marginTop: "8px",
-                                }}
-                            />
-                        </form>
-                    </div>
-                )}
             </div>
-        </>
+
+            {circles.map((circle, index) => (
+                <img
+                    key={index}
+                    src={circle.image}
+                    alt="Circle Image"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        handleCircleClick(index);
+                    }}
+                    className="absolute w-8 h-8 sm:w-10 sm:h-10 transform -translate-x-1/2 -translate-y-1/2"
+                    style={{
+                        left: `calc(50% + ${circle.position.x * 0.3}px)`,
+                        top: `calc(50% + ${circle.position.y * 0.3}px)`,
+                        cursor: "pointer",
+                    }}
+                />
+            ))}
+
+            {activeTextIndex !== null && (
+                <div
+                    className="absolute bg-white p-2 border border-gray-300 rounded shadow-lg text-xs sm:text-sm transform -translate-x-1/2 -translate-y-1/2"
+                    style={{
+                        left: `calc(50% + ${circles[activeTextIndex].position.x * 0.3}px)`,
+                        top: `calc(50% + ${circles[activeTextIndex].position.y * 0.3}px)`,
+                    }}
+                >
+                    {circles[activeTextIndex].text}
+                    <button
+                        onClick={() => setActiveTextIndex(null)}
+                        className="text-red-500 hover:text-red-700 ml-2"
+                    >
+                        X
+                    </button>
+                </div>
+            )}
+
+            {isModalOpen && (
+                <div
+                    className="absolute bg-white border border-gray-300 p-4 rounded shadow-lg transform -translate-x-1/2 -translate-y-1/2"
+                    style={{
+                        left: `calc(50% + ${modalPosition.x * 0.3}px)`,
+                        top: `calc(50% + ${modalPosition.y * 0.3}px)`,
+                        width: "90%",
+                        maxWidth: "300px",
+                    }}
+                >
+                    <button
+                        onClick={handleModalClose}
+                        className="text-gray-500 hover:text-gray-700 absolute top-2 right-2"
+                        style={{
+                            borderRadius: "50%",
+                        }}
+                    >
+                        X
+                    </button>
+                    <form onSubmit={onSubmitHandler}>
+                        <div>
+                            <input
+                                name="text"
+                                type="text"
+                                placeholder={t("message")}
+                                className="input w-full mt-4"
+                                value={text}
+                                onChange={handleTextChange}
+                            />
+                        </div>
+                        <input
+                            type="submit"
+                            value={t("submit")}
+                            className="btn w-full mt-2 bg-yellow-400 text-black rounded-lg cursor-pointer"
+                        />
+                    </form>
+                </div>
+            )}
+        </div>
     );
 }
-
